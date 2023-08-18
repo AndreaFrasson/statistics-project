92bcbdb0334a2eae1e3087d8b4293bf9a37623c6
Andrea Frasson <frass99and@gmail.com>
2023-08-18 11:43:25 +0200

    Add files via upload

--- corrandstrength.R
+++ corrandstrength.R
@@ -1,4 +1,4 @@
-library(MASS)
+library(MLDataR)
 library(tidymodels)
 library(tidyr)
 library(rpart)
@@ -8,9 +8,12 @@ library(ggplot2)
 library(geometry)
 
 #open dataset
-data("biopsy")
-df <- biopsy[-c(1)]
+data("stroke_classification")
+df <- stroke_classification[-c(1)]
 df <- na.omit(df)
+
+# Convert 1s to a positive label and 0s to a negative label
+df$stroke <- factor(ifelse(df$stroke == 1, "Positive", "Negative"))
 
 
 # Split the data into training and testing sets
@@ -18,49 +21,44 @@ data_split <- initial_split(df, prop = 0.75)
 train_data <- training(data_split)
 test_data <- testing(data_split)
 
-
 nf <- log2(ncol(df) - 1) + 1
 nt <- 50
 
-bio.rf <- randomForest(class ~ ., type = 'classification', data = train_data, 
-                        ntree = nt, mtry = nf, keep.forest = T, replace = T)
+bio.rf <- randomForest(stroke ~ ., type = 'classification', data = train_data, 
+                       importance=TRUE, ntree = nt, mtry = nf, replace = T, norm.votes = F)
 
-votes <- as.data.frame(bio.rf$votes / bio.rf$oob.times) #proportion OOB votes cast at x for each class
-mr <- c(votes['benign'] - votes['malignant'])[[1]] # margin for the random forest
 
-s1 <- 0
-s2 <- 0
+
+Qs <- as.data.frame(bio.rf$votes/bio.rf$oob.times) #proportion OOB votes cast at x for each class
+mr <- c(Qs['Negative'] - Qs['Positive'])[[1]] # margin for the random forest
+
 
 #correction on the sign and quantities for the correlation
 for(i in 1:nrow(train_data)){
-  if(train_data[i, 'class'] == 'malignant'){
+  if(train_data[i, 'stroke'] == 'Positive'){
     mr[i] <- mr[i] * - 1
-    s1 <- s1 + votes[i, 'malignant']
-    s2 <- s2 + votes[i, 'benign']
-  }
-  else {
-    s1 <- s1 + votes[i, 'benign']
-    s2 <- s2 + votes[i, 'malignant']
   }
 }
 
 #strength
 strength <- mean(mr)
-strength
-
 
 #E[quadro]
-squared.mr <- c((votes['benign'] - votes['malignant'])**2)[[1]]
+squared.mr <- c((Qs['Negative'] - Qs['Positive'])**2)[[1]]
 var.mr <- mean(squared.mr) - strength**2
 
-
-#proportion of right guesses
-p1 <- s1/nrow(train_data)
-#proportion of wrong guesses
-p2 <- s2/nrow(train_data)
-
-sd <- sqrt(p1 + p2 + (p1-p2)**2)
+sd_k <- c()
+for(i in 1:nrow(bio.rf$votes)){
+  p1 <- bio.rf$votes[i,'Positive']/bio.rf$oob.times
+  p2 <- bio.rf$votes[i, 'Negative']/bio.rf$oob.times
+  sd_k[i] <- sqrt(p1 + p2  + (p1 - p2)**2)
+}
 
 #correlation
-corr <- var.mr/(sd)**2
-corr
+corr <- var.mr/(mean(sd_k)**2)
+
+pe <- corr*(1 - strength**2)/strength**2
+
+y.hat <- predict(bio.rf, train_data[-c(1)])
+cm <- table(train_data$stroke, y.hat)
+as.numeric((cm[2,1] + cm[1,2])/sum(cm))
